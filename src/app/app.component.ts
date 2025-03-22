796bd3b2179817211ec6dc1f2c8c2d94e8c5a574
Habosa162 <habosa2000@hotmail.com>
2025-03-22 11:25:54 +0200

    firstCommit

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+@Component({
+  selector: 'app-root',
+  imports: [MatButtonModule, MatToolbarModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'hmsFE';
+}
+
+// bootstrapApplication(AppComponent, {
+//   providers: [provideAnimations()], // Required for Material animations
+// });
